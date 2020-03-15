ca7f2e4ed367f5a24f8946bebb3f144431fe4f6f
Giulio Presazzi <pgiulio@amazon.com>
2020-03-15 18:57:25 +0000

    Fixing line charts, improve readme

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
 Repo used to do some test with panda + altair.
 The used dataset comes from [COVID-19 public data sets](https://github.com/CSSEGISandData/COVID-19.git)
 
+This is a very simple project to generate some useful charts and represent how the COVID-19 is behaving
 
-### Installation
+## Examples 
+
+Example of histogram showing the number of people currently confirmed
+![histogram](https://github.com/gpresazzi/covid19-data-visualization/blob/master/images/histogram_total.png)
+
+Example of line chart for the confirmed infected people by day
+![line chart](https://github.com/gpresazzi/covid19-data-visualization/blob/master/images/line_chart.png)
+
+
+
+## Installation
 
 - 1 - Clone this repo
 ```bash
--- src/covid_confirmed.py
+++ src/covid_confirmed.py
@@ -26,7 +26,7 @@ class CovidConfirmed:
 
         chart = alt.Chart(chart_data).mark_bar().encode(
             x=alt.X(self.col_name_countries, sort='-x'),
-            y=alt.Y(day_str)
+            y=alt.Y(day_str, axis=alt.Axis(format='N', title='TOT confirmed cases {}'.format(day_str)))
         ).interactive()
         return chart
 
@@ -40,70 +40,65 @@ class CovidConfirmed:
 
         chart = alt.Chart(chart_data).mark_bar().encode(
             x=alt.X(self.col_name_countries, sort='-x'),
-            y=alt.Y(col_name_increments, axis=alt.Axis(format='N', title='daily increments'))
+            y=alt.Y(col_name_increments, axis=alt.Axis(format='N', title='daily increments on {}'.format(day_str)))
         ).interactive()
         return chart
 
     def get_chart_increment_per_day(self, day_str, num_previous_days=30):
         col_name_date = "date"
-        col_name_value = "value"
+        col_name_value = "increments per day"
 
-        previous_day = DateHelper.previous_day_str(day_str)
         data_1 = pandas.DataFrame()
-        data_1[self.col_name_countries] = self._data[self.col_name_countries]
-        data_1[col_name_date] = pandas.to_datetime(day_str, format='%m/%d/%y', errors='ignore')
-        data_1[col_name_value] = self._data[day_str] - self._data[previous_day]
-        data_1 = data_1.groupby([self.col_name_countries, "date"], as_index=False).agg({col_name_value: "sum"})
-        print(data_1.head())
-
         current_date_str = day_str
         for x in range(num_previous_days):
             previous_day = DateHelper.previous_day_str(current_date_str)
 
             data_tmp = pandas.DataFrame()
             data_tmp[self.col_name_countries] = self._data[self.col_name_countries]
-            data_tmp[col_name_date] = pandas.to_datetime(previous_day, format='%m/%d/%y', errors='ignore')
-            data_tmp[col_name_value] = self._data[day_str] - self._data[previous_day]
-            data_tmp = data_tmp.groupby([self.col_name_countries, "date"], as_index=False).agg({col_name_value: "sum"})
+            data_tmp[col_name_date] = pandas.to_datetime(current_date_str, format='%m/%d/%y', errors='ignore')
+            data_tmp[col_name_value] = self._data[current_date_str] - self._data[previous_day]
+            data_tmp = data_tmp.groupby([self.col_name_countries, col_name_date], as_index=False).agg({col_name_value: "sum"})
 
             data_1 = pandas.concat([data_1, data_tmp], ignore_index=True)
             current_date_str = previous_day
 
-        highlight = alt.selection(type='single', on='mouseover', fields=[self.col_name_countries], nearest=True)
+        nearest = alt.selection(type='single', nearest=True, on='mouseover',
+                                fields=['date'], empty='none')
         line = alt.Chart(data_1).mark_line().encode(
-            y=alt.Y("value:Q"),
-            x=alt.X('date:T', axis=alt.Axis(title='Date'.upper(), format="%d/%m/%Y")),
-            color='{}:N'.format(self.col_name_countries)
+            y=alt.Y("{}:Q".format(col_name_value)),
+            x=alt.X('{}:T'.format(col_name_date), axis=alt.Axis(title='Date'.upper(), format="%d/%m/%Y")),
+            color='{}'.format(self.col_name_countries),
+            tooltip='{}'.format(self.col_name_countries)
         )
 
         selectors = alt.Chart(data_1).mark_point().encode(
-            x=alt.X('date:T'),
+            x=alt.X('{}:T'.format(col_name_date), axis=alt.Axis(title='Date'.upper(), format="%d/%m/%Y")),
             opacity=alt.value(0),
         ).add_selection(
-            highlight
+            nearest
         )
 
         # Draw points on the line, and highlight based on selection
         points = line.mark_point().encode(
-            opacity=alt.condition(highlight, alt.value(1), alt.value(0))
+            opacity=alt.condition(nearest, alt.value(1), alt.value(0))
         )
 
         # Draw text labels near the points, and highlight based on selection
         text = line.mark_text(align='left', dx=5, dy=-5).encode(
-            text=alt.condition(highlight, "value:Q", alt.value(' '))
+            text=alt.condition(nearest, '{}'.format(self.col_name_countries), alt.value(' '))
         )
 
         # Draw a rule at the location of the selection
         rules = alt.Chart(data_1).mark_rule(color='gray').encode(
-            x=alt.X('date:T'),
+            x=alt.X('{}:T'.format(col_name_date)),
         ).transform_filter(
-            highlight
+            nearest
         )
 
         chart = alt.layer(
             line, selectors, points, rules, text
         ).properties(
-            width=600, height=300
+            width=800, height=500
         ).interactive()
 
         return chart
