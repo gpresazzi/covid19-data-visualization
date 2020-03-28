a50347284dffba8be7ffd060858b6b3cb50358e5
Giulio Presazzi <pgiulio@amazon.com>
2020-03-28 16:01:37 +0000

    Add html template and save as json

--- generate_charts.sh
+++ generate_charts.sh
@@ -5,6 +5,18 @@ echo $dir
 
 cd "$dir/COVID-19" && git pull
 cd "$dir"
-python3 src/main.py death -f html -o output/deaths.html
-python3 src/main.py confirmed -f html -o output/confirmed.html
-python3 src/main.py recovered -f html -o output/recovered.html
+
+# Generate HTML versions
+# python3 src/main.py death -f html -o output/deaths.html
+# python3 src/main.py confirmed -f html -o output/confirmed.html
+# python3 src/main.py recovered -f html -o output/recovered.html
+
+# Generate JSON versions
+python3 src/main.py death -f json -o output/deaths.json
+python3 src/main.py confirmed -f json -o output/confirmed.json
+python3 src/main.py recovered -f json -o output/recovered.json
+
+# Using template to generate HTML
+sed 's/chart.json/deaths.json/g' html/template.html > output/deaths.html
+sed 's/chart.json/deaths.json/g' html/template.html > output/confirmed.html
+sed 's/chart.json/deaths.json/g' html/template.html > output/confirmed.html
