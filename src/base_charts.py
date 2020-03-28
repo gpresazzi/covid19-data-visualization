2ed85766338105ab6548205cce6e7d3a6b7783c7
Giulio Presazzi <pgiulio@amazon.com>
2020-03-28 17:14:35 +0000

    Add global trending

--- src/base_charts.py
+++ src/base_charts.py
@@ -7,7 +7,7 @@ from date_helper import DateHelper
 class BaseCharts:
     col_name_countries = "Country/Region"
 
-    def __init__(self, csv_file_path, num_countries=20):
+    def __init__(self, csv_file_path, num_countries=15):
         dir_path = os.getcwd()
         self.num_countries = num_countries
         csv_data = os.path.join(dir_path, csv_file_path)
@@ -79,7 +79,81 @@ class BaseCharts:
         line = alt.Chart(data_1).mark_line().encode(
             y=alt.Y("{}:Q".format(col_name_value)),
             x=alt.X('{}:T'.format(col_name_date), axis=alt.Axis(title='Date'.upper(), format="%d/%m/%Y")),
-            color=alt.Color('{}'.format(self.col_name_countries), scale=alt.Scale(scheme='category20c'), 
+            color=alt.Color('{}'.format(self.col_name_countries), scale=alt.Scale(scheme='category20c'),
+                            legend=alt.Legend(orient='right')),
+            tooltip='{}'.format(self.col_name_countries),
+        )
+
+        selectors = alt.Chart(data_1).mark_point().encode(
+            x=alt.X('{}:T'.format(col_name_date), axis=alt.Axis(title='Date'.upper(), format="%d/%m/%Y")),
+            opacity=alt.value(0),
+        ).add_selection(
+            nearest
+        )
+
+        # Draw points on the line, and highlight based on selection
+        points = line.mark_point().encode(
+            opacity=alt.condition(nearest, alt.value(1), alt.value(0))
+        )
+
+        # Draw text labels near the points, and highlight based on selection
+        text = line.mark_text(align='left', dx=5, dy=-5).encode(
+            text=alt.condition(nearest, '{}'.format(self.col_name_countries), alt.value(' '))
+        )
+
+        # Draw a rule at the location of the selection
+        rules = alt.Chart(data_1).mark_rule(color='gray').encode(
+            x=alt.X('{}:T'.format(col_name_date)),
+        ).transform_filter(
+            nearest
+        ).properties(
+            title=title
+        )
+
+        chart = alt.layer(
+            line, selectors, points, rules, text
+        ).properties(
+            width=800, height=500
+        ).interactive()
+
+        return chart
+
+    def get_chart_per_day(self, day_str, num_previous_days=55, title=""):
+        col_name_date = "date"
+        col_name_value = "increments per day"
+
+        # Get the top affected countries
+        top_countries_data = self._data.groupby(self.col_name_countries, as_index=False).agg({day_str: "sum"})
+        top_countries = top_countries_data.sort_values(
+            by=[day_str], ascending=False
+        )[:self.num_countries][self.col_name_countries]
+
+        # print(top_countries)
+        # Filter only the top countries data
+        chart_data = self._data[self._data["Country/Region"].isin(top_countries.values)]
+        # print(chart_data)
+
+        data_1 = pandas.DataFrame()
+        current_date_str = day_str
+        for x in range(num_previous_days):
+            previous_day = DateHelper.previous_day_str(current_date_str)
+
+            data_tmp = pandas.DataFrame()
+            data_tmp[self.col_name_countries] = chart_data[self.col_name_countries]
+            data_tmp[col_name_date] = pandas.to_datetime(current_date_str, format='%m/%d/%y', errors='ignore')
+            data_tmp[col_name_value] = chart_data[current_date_str]
+            data_tmp = data_tmp.groupby([self.col_name_countries, col_name_date], as_index=False).agg(
+                {col_name_value: "sum"}
+            )
+
+            data_1 = pandas.concat([data_1, data_tmp], ignore_index=True)
+            current_date_str = previous_day
+
+        nearest = alt.selection(type='single', nearest=True, on='mouseover', fields=['date'], empty='none')
+        line = alt.Chart(data_1).mark_line().encode(
+            y=alt.Y("{}:Q".format(col_name_value)),
+            x=alt.X('{}:T'.format(col_name_date), axis=alt.Axis(title='Date'.upper(), format="%d/%m/%Y")),
+            color=alt.Color('{}'.format(self.col_name_countries), scale=alt.Scale(scheme='category20c'),
                             legend=alt.Legend(orient='right')),
             tooltip='{}'.format(self.col_name_countries),
         )
--- src/main.py
+++ src/main.py
@@ -48,8 +48,10 @@ def main():
 
     map_chart = map_builder.get_map(date_str, f"Worldwide map of {args.command} patients")
 
+    chart_lines_tot = charts.get_chart_per_day(date_str, title="Total in top 20 countries")
+
     chart_row = alt.hconcat(chart_tot_per_day, chart_increment_last)
-    chart = alt.vconcat(map_chart, chart_lines_incr, chart_row)
+    chart = alt.vconcat(map_chart, chart_lines_tot, chart_lines_incr, chart_row)
 
     if args.format == OutputFormat.html:
         if not os.path.exists(output_dir):
